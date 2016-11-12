2be2d83ff5d80d54104c2191ca64552c3116a600
edelCustodio <edelcustodiofrias@gmail.com>
2016-11-12 10:43:51 -0500

    Frist commit FriasoftIt

---
+++ Controllers/ServiciosController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FriasoftIt.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Desarrollo()
+        {
+            return View();
+        }
+
+        public IActionResult Negocios()
+        {
+            return View();
+        }
+
+        public IActionResult Movil()
+        {
+            return View();
+        }
+
+        public IActionResult UiUx()
+        {
+            return View();
+        }
+    }
+}
